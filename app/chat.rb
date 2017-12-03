86568cd0b4c7e8662401a933be8f6e05d8741b0c
mur71mur <fullsawa58@gmail.com>
2017-12-04 04:58:45 +0900

    チャットたんメソッド

--- app/chat.rb
+++ app/chat.rb
@@ -2,10 +2,14 @@ require 'json'
 require 'rest-client'
 require "cgi"
 
-request_content = {'key' => 'sample', 'message' => CGI.escape('こんにちは')}
-request_params = request_content.reduce([]) do |params, (key, value)|
-  params << "#{key.to_s}=#{value}"
+module chatbot-api
+  def chat(key,message)
+    request_content = {'key' => key, 'message' => CGI.escape(message)}
+    request_params = request_content.reduce([]) do |params, (key, value)|
+      params << "#{key.to_s}=#{value}"
+    end
+    rest = RestClient.get('https://chatbot-api.userlocal.jp/api/chat?' + request_params.join('&').to_s)
+    result = JSON.parse(rest)
+    return result['result']
+  end
 end
-rest = RestClient.get('https://chatbot-api.userlocal.jp/api/chat?' + request_params.join('&').to_s)
-result = JSON.parse(rest)
-p result['result']
